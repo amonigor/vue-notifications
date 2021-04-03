b60570b4ca63fd2f851a3d6439d328ca4e76a260
Igor Amon <48227869+amonigor@users.noreply.github.com>
2021-04-03 16:18:20 -0300

    Created the components of notification

---
+++ src/components/Notification.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="vn-notification" :style="vnVars" @click.prevent="vnRedirect">
+        <div class="vn-header">
+            <img v-if="icon" :src="icon" />
+            <p>{{ title }}</p>
+        </div>
+        <div class="vn-body">
+            <img src="@/assets/logo.png" />
+            <p>
+                Duis nulla amet elit proident et fugiat dolor id excepteur Lorem
+                sint exercitation magna.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Notification",
+    props: {
+        // Values props
+        title: {
+            type: String,
+            required: true,
+            default: "Vue Notification",
+        },
+        icon: {
+            type: String,
+            required: false,
+        },
+
+        // Styling props
+        mainColor: {
+            type: String,
+            required: false,
+            default: "#ffffff",
+        },
+        backgroundColor: {
+            type: String,
+            required: false,
+            default: "#cecece",
+        },
+    },
+    computed: {
+        vnVars() {
+            return {
+                "--main-color": this.mainColor,
+                "--background-color": this.backgroundColor,
+                "--border-radius": "10px",
+            };
+        },
+    },
+    methods: {
+        vnRedirect() {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.vn-notification {
+    width: 350px;
+    padding: 10px;
+    border-radius: 5px;
+    background-color: var(--background-color);
+    cursor: pointer;
+    opacity: 0.9;
+    -webkit-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.4);
+    -moz-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.4);
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.4);
+    transition-duration: 0.2s;
+
+    &:hover {
+        opacity: 1;
+    }
+
+    .vn-header,
+    .vn-body {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        width: 100%;
+    }
+
+    .vn-header {
+        justify-content: flex-start;
+
+        img {
+            width: 20px;
+            height: 20px;
+            margin-right: 10px;
+        }
+
+        p {
+            font-weight: bold;
+            font-size: 0.75em;
+            opacity: 0.75;
+        }
+    }
+
+    .vn-body {
+        margin-top: 10px;
+        justify-content: flex-start;
+
+        img {
+            width: 50px;
+            height: 50px;
+            border-radius: 100%;
+            object-fit: cover;
+            margin-right: 10px;
+            background-color: #000000;
+        }
+
+        p {
+            text-align: left;
+            font-size: 0.9em;
+        }
+    }
+}
+</style>
---
+++ src/components/NotificationsBox.vue
@@ -0,0 +1,117 @@
+<template>
+    <div :class="`vn-box vn-${position}`">
+        <transition-group name="vn-fade" mode="out-in">
+            <Notification
+                v-for="(notification, idx) in notifications"
+                :key="(idx+1)"
+                :title="notification.title"
+                :icon="notification.icon"
+            />
+        </transition-group>
+    </div>
+</template>
+
+<script>
+import Notification from "./Notification";
+
+export default {
+    name: "NotificationsBox",
+    components: { Notification },
+    props: {
+        position: {
+            type: String,
+            required: false,
+            default: "top-center",
+        },
+        notifications: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+.vn-box {
+    display: flex;
+    position: absolute;
+
+    &.vn-top-center,
+    &.vn-bottom-center {
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    &.vn-top-center,
+    &.vn-top-left,
+    &.vn-top-right {
+        flex-direction: column-reverse;
+        margin-top: 15px;
+
+        .vn-notification {
+            margin-bottom: 10px;
+        }
+    }
+
+    &.vn-bottom-center,
+    &.vn-bottom-left,
+    &.vn-bottom-right {
+        flex-direction: column;
+        bottom: 0;
+        margin-top: 0 !important;
+        margin-bottom: 15px !important;
+
+        .vn-notification {
+            margin-top: 10px;
+        }
+    }
+
+    &.vn-top-left,
+    &.vn-bottom-left {
+        left: 0;
+        margin: 15px 0 0 15px;
+    }
+
+    &.vn-top-right,
+    &.vn-bottom-right {
+        right: 0;
+        margin: 15px 15px 0 0;
+    }
+}
+
+.vn-fade-enter-active,
+.vn-fade-leave-active {
+    transition: opacity 0.3s;
+}
+
+.vn-fade-enter,
+.vn-fade-leave-to {
+    opacity: 0;
+}
+
+@media only screen and (max-width: 500px) {
+    .vn-box {
+        justify-content: center;
+
+        &.vn-top-center,
+        &.vn-top-left,
+        &.vn-top-right,
+        &.vn-bottom-center,
+        &.vn-bottom-left,
+        &.vn-bottom-right {
+            margin-left: 15px;
+            margin-right: 15px;
+
+            .vn-notification {
+                width: 100%;
+            }
+        }
+
+        &.vn-top-center,
+        &.vn-bottom-center {
+            left: 0;
+            transform: translateX(0);
+        }
+    }
+}
+</style>
